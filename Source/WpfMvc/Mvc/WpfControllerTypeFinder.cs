af4d9a29cc2ccca54efc7a4766a5e8c15d41d8ed
averrunci <averrunci@fievus.com>
2023-07-31 11:59:19 +0900

    Improve a performance for the WpfControllerTypeFinder.

--- Source/WpfMvc/Mvc/WpfControllerTypeFinder.cs
+++ Source/WpfMvc/Mvc/WpfControllerTypeFinder.cs
@@ -1,7 +1,8 @@
-﻿// Copyright (C) 2022 Fievus
+﻿// Copyright (C) 2022-2023 Fievus
 //
 // This software may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
+using System.Reflection;
 using System.Windows;
 
 namespace Charites.Windows.Mvc;
@@ -13,5 +14,9 @@ internal sealed class WpfControllerTypeFinder : ControllerTypeFinder<FrameworkEl
     }
 
     protected override IEnumerable<Type> FindControllerTypeCandidates(FrameworkElement view)
-        => AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes());
+        => controllerTypeCandidates ??= AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(t => t.GetTypeInfo().GetCustomAttributes<ViewAttribute>(true).Any())
+            .ToList();
+    private IEnumerable<Type>? controllerTypeCandidates;
 }
